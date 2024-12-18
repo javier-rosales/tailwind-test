8453332e50d73e11f07ceef631527f8ce88d6ceb
Javier Rosales <cyjav.dev@gmail.com>
2024-12-18 14:45:17 -0600

    Update component (Section): Make padding responsive

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,6 +1,6 @@
 const Section = ({ children }) => {
   return (
-    <section className='max-w-7xl mx-auto px-4 md:px-7 lg:px-8 py-10 lg:py-16 text-lg md:text-xl'>
+    <section className='max-w-7xl mx-auto px-4 md:px-7 lg:px-8 py-8 md:py-10 lg:py-16 text-lg md:text-xl'>
       {children}
     </section>
   )
