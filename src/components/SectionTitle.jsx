c709d56b665ce57dac9db59096624efe6f7d58b9
Javier Rosales <cyjav.dev@gmail.com>
2024-12-20 11:28:21 -0600

    Update component(SectionTitle): Add larger margin bottom on large screens

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -1,6 +1,6 @@
 const SectionTitle = ({ children }) => {
   return (
-    <h3 className='text-4xl font-medium py-4 mb-3 md:mb-5 md:text-5xl'>
+    <h3 className='text-4xl font-medium py-4 mb-3 md:mb-5 lg:mb-7 md:text-5xl'>
       {children}
     </h3>
   )
